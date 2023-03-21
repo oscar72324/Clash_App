ab4059c1e15aaf0281eafc9163f168914bd28ccc
Oscar <Oscaribarra72@hotmail.com>
2023-03-21 08:50:33 -0700

    testing

--- pages/cards.jsx
+++ pages/cards.jsx
@@ -1,49 +1,53 @@
 import Image from "next/image";
 import Link from "next/link";
-
-export async function getServerSideProps() {
-  try {
-    const token = process.env.CLASH_ROYALE_API;
-
-    const clashApi = await fetch("https://api.clashroyale.com/v1/cards", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-      },
-    });
-
-    const clash = await clashApi.json();
-
-    if (!clash || !clash.items) {
-      throw new Error("No items found");
-    }
-
-    return {
-      props: {
-        clashCard: clash,
-      },
-    };
-  } catch (error) {
-    console.error(error);
-    return {
-      props: {
-        clashCard: null,
-      },
-    };
-  }
-}
+import { useEffect, useState } from "react";
 
 export default function Cards(props) {
-  if (!props.clashCard) {
-    return <div>Error: Failed to fetch data</div>;
+  const [clashCard, setClashCard] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchClashCards = async () => {
+      try {
+        const token = process.env.CLASH_ROYALE_API;
+
+        const clashApi = await fetch("https://api.clashroyale.com/v1/cards", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+          },
+        });
+
+        const clash = await clashApi.json();
+
+        if (!clash || !clash.items) {
+          throw new Error("No items found");
+        }
+
+        setClashCard(clash);
+      } catch (error) {
+        console.error(error);
+        setError("Failed to fetch data");
+      }
+    };
+
+    fetchClashCards();
+  }, []);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  if (!clashCard) {
+    return <div>Loading...</div>;
   }
 
   return (
     <div className="flex justify-center py-20">
       <div className="flex flex-wrap gap-4 p-4 justify-center rounded-lg items-center text-center w-[1000px] bg-white ">
-        {props.clashCard.items.map((card) => (
+        {clashCard.items.map((card) => (
           <Link
             key={card.id}
             className="flex flex-col h-[150px] font-bold"
