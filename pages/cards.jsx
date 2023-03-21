23a268e9faf0c800775554c0bfb6e50a3168db4e
Oscar <Oscaribarra72@hotmail.com>
2023-03-21 08:57:16 -0700

    back to original

--- pages/cards.jsx
+++ pages/cards.jsx
@@ -1,52 +1,46 @@
 import Image from "next/image";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+
+export async function getServerSideProps() {
+  try {
+    const token = process.env.CLASH_ROYALE_API;
+
+    const clashApi = await fetch("https://api.clashroyale.com/v1/cards", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const clash = await clashApi.json();
+
+    if (!clash || !clash.items) {
+      throw new Error("No items found");
+    }
+
+    return {
+      props: {
+        clashCard: clash,
+      },
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      props: {
+        clashCard: null,
+      },
+    };
+  }
+}
 
 export default function Cards(props) {
-  const [clashCard, setClashCard] = useState(null);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchClashCards = async () => {
-      try {
-        const token = process.env.CLASH_ROYALE_API;
-
-        const clashApi = await fetch("https://api.clashroyale.com/v1/cards", {
-          headers: {
-            Authorization: `Bearer ${token}`,
-            'Access-Control-Allow-Origin': '*',
-
-          },
-        });
-
-        const clash = await clashApi.json();
-
-        if (!clash || !clash.items) {
-          throw new Error("No items found");
-        }
-
-        setClashCard(clash);
-      } catch (error) {
-        console.error(error);
-        setError("Failed to fetch data");
-      }
-    };
-
-    fetchClashCards();
-  }, []);
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
-  if (!clashCard) {
-    return <div>Loading...</div>;
+  if (!props.clashCard) {
+    return <div>Error: Failed to fetch data</div>;
   }
 
   return (
     <div className="flex justify-center py-20">
       <div className="flex flex-wrap gap-4 p-4 justify-center rounded-lg items-center text-center w-[1000px] bg-white ">
-        {clashCard.items.map((card) => (
+        {props.clashCard.items.map((card) => (
           <Link
             key={card.id}
             className="flex flex-col h-[150px] font-bold"
